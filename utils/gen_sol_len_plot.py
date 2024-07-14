58f86743facb957c86ffe1da469823f6d9fc4d7a
Paul <pkroeger@student.ethz.ch>
2024-07-15 01:33:23 +0200

    Added comparison for solution lengths.

--- utils/gen_sol_len_plot.py
+++ utils/gen_sol_len_plot.py
@@ -10,8 +10,8 @@ import matplotlib.pyplot as plt
 
 plt.rcParams['font.family'] = 'DejaVu Serif'
 
-path = "Results/CBC/solution_lengths.csv"
-# path = "/Results/Microban/solution_lengths.csv"
+# path = "Results/CBC/solution_lengths.csv"
+path = "Results/Microban/solution_lengths.csv"
 df = pd.read_csv(path, index_col=None)
 level_ids = df.iloc[:, 0].values
 sol_diffs = df.iloc[:, 1].values - df.iloc[:, 2].values
@@ -20,8 +20,8 @@ mean = np.mean(sol_diffs)
 for i in range(0, max(level_ids), 5):
     plt.axvline(x=i, color='lightblue', linestyle='--')
 plt.scatter(level_ids, sol_diffs, marker='x', color='b')
-plt.axhline(y=mean, color='r', linestyle='--', label=f'Mean: {mean:.2f}') 
+plt.axhline(y=mean, color='r', linestyle='--', label=f'Mean: {mean:.2f}')
 plt.xlabel("Level Id")
-plt.ylabel("Number Box Pushes Schokoban - Number Box Pushes Vanillaban")
-plt.legend()
+plt.ylabel("Box Pushes Schokoban - Box Pushes Vanillaban")
+plt.legend(loc='lower left')
 plt.show()
